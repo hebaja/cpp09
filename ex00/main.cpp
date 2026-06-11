8393a58f02804f6e91879c13af21ae2a87130142
hebatist <hebaja@yahoo.com.br>
2026-06-10 21:52:55 -0300

    ex00: reading from input.txt file

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,19 +2,50 @@
 #include <iostream>
 #include <string>
 
-int	main()
+int	main(int argc, char **argv)
 {
-	std::ifstream	baseFile("data.csv");
-
-	if (!baseFile.is_open())
 	{
-		std::cerr << "Error: could not open csv file" << std::endl;
-		return (1);
+		if (argc == 2)
+		{
+			std::ifstream	inputFile(argv[1]);
+		
+			if (!inputFile.is_open())
+			{
+				std::cerr << "Error: could not open input file." << std::endl;
+				return (1);
+			}
+			else
+			{
+				std::string	line;
+				while (std::getline(inputFile, line))
+					std::cout << line << std::endl;
+
+				inputFile.close();
+			}
+		}
+		else
+		{
+			std::cerr << "Error: could not open file." << std::endl;
+			return (1);
+		}
+			
 	}
+	/*
+	{
+		std::ifstream	baseFile("data.csv");
 
-	std::string	line;
-	while (std::getline(baseFile, line))
-		std::cout << line << std::endl;
+		if (!baseFile.is_open())
+		{
+			std::cerr << "Error: could not open csv file." << std::endl;
+			return (1);
+		}
 
+		std::string	line;
+		while (std::getline(baseFile, line))
+			std::cout << line << std::endl;
+
+		baseFile.close();
+	}
+	*/
 	return (0);
 }
