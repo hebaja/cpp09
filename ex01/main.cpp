eb8a6b7d583aecc620a15b50a0488e82c572d5b8
Henrique <hebaja@yahoo.com.br>
2026-06-19 01:17:09 -0300

    ex01 expression parsing improved

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,6 +4,17 @@
 #include <stdexcept>
 #include <string>
 
+bool	validateToken(std::string::iterator &next, std::string::iterator end)
+{
+	++next;
+	if (next != end && *next != ' ')
+	{
+		std::cerr << "Error: malformed RPN expression" << std::endl;
+		return (false);
+	}
+	return (true);
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
@@ -15,10 +26,19 @@ int	main(int argc, char **argv)
 		try {
 			for (std::string::iterator it = input.begin(); it != input.end(); ++it)
 			{
+				std::string::iterator	next = it;
 				if (std::isdigit(static_cast<unsigned char>(*it)))
+				{
+					if (!validateToken(next, input.end()))
+						return (1);
 					rpn.add(*it);
+				}
 				else if (oper.find_first_of(*it) != std::string::npos)
+				{
+					if (!validateToken(next, input.end()))
+						return (1);
 					rpn.calculate(*it);
+				}
 				else if (*it == ' ')
 					continue ;
 				else
@@ -31,7 +51,6 @@ int	main(int argc, char **argv)
 		} catch (std::runtime_error &e) {
 			std::cerr << e.what() << std::endl;
 		}
-
 	}
 	else
 		std::cerr << "you must input an RPN expression" << std::endl;
