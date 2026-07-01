e53f0fd2291a0e4f8faf029a58db168ccbc22d90
Henrique <hebaja@yahoo.com.br>
2026-07-01 00:22:59 -0300

    ex00 fix algorithm to work with pairing correctly

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -1,45 +1,48 @@
-#pragma  once
+#pragma once
 
 #include <deque>
-#include <vector>
 #include <iostream>
+#include <vector>
 
-struct Pair
-{
-	unsigned int	a;
-	unsigned int	b;
+struct Pair {
+  unsigned int a;
+  unsigned int b;
 
-	unsigned int operator[](unsigned int index) const {
-		if (index == 0)
-			return (a);
-		return (b);
-	}
-	
-	unsigned int &operator[](unsigned int index) {
-		if (index == 0)
-			return (a);
-		return (b);
-	}
+  unsigned int operator[](unsigned int index) const {
+    if (index == 0)
+      return (a);
+    return (b);
+  }
+
+  unsigned int &operator[](unsigned int index) {
+    if (index == 0)
+      return (a);
+    return (b);
+  }
+
+  bool operator<(const Pair &other) const { return (a < other.a); }
 };
 
 inline std::ostream &operator<<(std::ostream &os, const Pair &p) {
-	os << p[0] << " " << p[1];
-	return os;
+  os << p[0] << " " << p[1];
+  return os;
 }
 
-class PmergeMe
-{
+typedef std::vector<unsigned int> UintVec;
+typedef std::deque<unsigned int> UintDeq;
+
+class PmergeMe {
 private:
-	std::vector<unsigned int>	_alist;
-	std::deque<unsigned int>	_blist;
+  std::vector<unsigned int> _alist;
+  std::deque<unsigned int> _blist;
 
 public:
-	PmergeMe();
-	PmergeMe(int argc, char **argv);
-	PmergeMe(const PmergeMe &other);
-	PmergeMe& operator=(const PmergeMe &other);
-	~PmergeMe();
+  PmergeMe();
+  PmergeMe(int argc, char **argv);
+  PmergeMe(const PmergeMe &other);
+  PmergeMe &operator=(const PmergeMe &other);
+  ~PmergeMe();
 
-	void	showBeforeSort();
-	void	showAfterSort();
+  void showBeforeSort();
+  void showAfterSort();
 };
